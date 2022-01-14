a66dbc30f5a13b1ad1c4c4732b5d708b8acae460
rafestorn <rafestorn@alum.us.es>
2022-01-14 12:24:47 +0100

    Examen lab

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
@@ -2,21 +2,41 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.samples.petclinic.pet.PetType;
+import org.springframework.samples.petclinic.pet.exceptions.DuplicatedPetNameException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class FeedingService {
+	
+	@Autowired
+	private FeedingRepository fr;
+	
     public List<Feeding> getAll(){
-        return null;
+        return fr.findAll();
     }
 
     public List<FeedingType> getAllFeedingTypes(){
-        return null;
+        return fr.findAllFeedingTypes();
     }
 
     public FeedingType getFeedingType(String typeName) {
-        return null;
+        return fr.findFeedingTypeByName(typeName);
     }
 
-    public Feeding save(Feeding p) throws UnfeasibleFeedingException {
-        return null;       
+    @Transactional(rollbackFor = UnfeasibleFeedingException.class)
+    public Feeding save(Feeding p) throws DataAccessException, UnfeasibleFeedingException {
+    	PetType pt1 = p.getPet().getType();
+    	PetType pt2 = p.getFeedingType().getPetType();
+    	if(pt1!=pt2) {
+    		throw new UnfeasibleFeedingException();
+    	}else {
+    		fr.save(p);
+    	}
+        return p;       
     }
 
     
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingTypeFormatter.java
@@ -3,22 +3,29 @@ package org.springframework.samples.petclinic.feeding;
 import java.text.ParseException;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FeedingTypeFormatter implements Formatter<FeedingType>{
 
+	@Autowired 
+	private FeedingService fs;
+	
     @Override
     public String print(FeedingType object, Locale locale) {
-        // TODO Auto-generated method stub
-        return null;
+        return object.getName();
     }
 
     @Override
     public FeedingType parse(String text, Locale locale) throws ParseException {
-        // TODO Auto-generated method stub
-        return null;
+        FeedingType ft = fs.getFeedingType(text);
+        if(ft==null) {
+        	throw new ParseException("Type not found: "+text, 0);
+        }else {
+        	return ft;
+        }
     }
     
 }
